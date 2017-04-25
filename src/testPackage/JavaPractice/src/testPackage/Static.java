e2b10f1f3bc0c25d49ea137e46336b68294fbb7a
George Nguyen <g.nguyen92@yahoo.com>
2017-04-25 13:16:51 -0700

    Flesh out Static class with instance variables and static variables.

--- src/testPackage/JavaPractice/src/testPackage/Static.java
+++ src/testPackage/JavaPractice/src/testPackage/Static.java
@@ -1,5 +1,24 @@
 package testPackage;
 
+/**
+ * Static variables can be used when all objects
+ * in a class share a common variable.
+ * 	Ex)
+ * An organization of women all have different member variables (names)
+ * but all contribute to the total number of members.
+ * @author gnguy
+ *
+ */
 public class Static {
-
+	private String firstName;
+	private String lastName;
+	private static int numMembers = 0;
+	
+	public Static(String fn, String ln){
+		firstName = fn;
+		lastName = ln;
+		numMembers++;
+		System.out.printf("%s %s is a member in the total "
+				+ "organization size of %i", firstName, lastName, numMembers);
+	}
 }
